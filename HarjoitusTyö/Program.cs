b2ff4bbd60685581b708f7ac54a03abbf7705fb0
Tuukka Tenhunen <tuukka-tapio.tenhunen@outlook.com>
2021-11-26 14:28:26 +0200

    Add project files.

---
+++ HarjoitusTyö/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HarjoitusTyö
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
