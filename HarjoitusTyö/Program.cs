94afcefeca24e5c88dda167c8789763dac6d8a6a
Tuukka Tenhunen <tuukka-tapio.tenhunen@outlook.com>
2021-12-01 14:04:56 +0200

    booli

--- HarjoitusTyö/Program.cs
+++ HarjoitusTyö/Program.cs
@@ -8,19 +8,14 @@ namespace HarjoitusTyö
 
         static void Main(string[] args)
         {
-            bool InCombat = false;
+
             Character player = new Character(10, 2, 1);
 
             Console.WriteLine("Insert your name.");
             player.SetPlayerName();
             Console.WriteLine($"{player.PlayerName}'s stats: \nHP: {player.PlayerHealth.ToString()} \nDAMAGE: {player.PlayerDamageBase.ToString()} \nLevel {player.PlayerLevel.ToString()}");
 
-
-            while(InCombat == true)
-            {
-
-            }
-            
+        
         }
     }
 }
