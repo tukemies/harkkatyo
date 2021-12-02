918ed77799e26857b9715f7b76043fcf6b04a7a8
Tomi Oksman <tomioksman@kamk.fi>
2021-12-02 15:03:34 +0200

    Interface

---
+++ HarjoitusTyö/IAction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HarjoitusTyö
+{
+    interface IAction
+    {
+        public void Action(Character lAttack, Character hAttack);
+    }
+}
